160f311f4f7a45cf2c0142bd87ab21bc24f7e33d
axel.bertrand@etu.univ-lyon1.fr <axel.bertrand@etu.univ-lyon1.fr>
2016-02-10 11:06:17 +0100

    Première itération commencée, ajout de twig, de la route et de la vue de l'index, ajout préventif de Doctrine (penser à faire composer update)

--- web/index.php
+++ web/index.php
@@ -4,12 +4,8 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function () {
-    return 'Hello world';
-});
-
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello ' . $app->escape($name);
-});
+require __DIR__.'/../app/config/dev.php';
+require __DIR__.'/../app/app.php';
+require __DIR__.'/../app/routes.php';
 
 $app->run();
